70df4330ef530c1be1b6a73702aefc0bce97f336
amirmanshadi <amirmanshadi.code@gmail.com>
2023-04-02 17:12:32 +0430

    added About.js page

---
+++ src/pages/About.js
@@ -0,0 +1,32 @@
+const About = () => {
+	return (
+		<>
+			<h1>about page</h1>
+			<p>
+				Eum nulla quisquam debitis ex id reiciendis aut. Et iste voluptatibus
+				omnis aliquam. Ut id recusandae autem.
+			</p>
+			<p>
+				Deleniti aspernatur aliquid quia doloremque officiis. Impedit non
+				cupiditate tempore veritatis ex quae est. At nihil quas est vitae ut
+				molestiae repellat excepturi. Temporibus tempora sit nobis. Eum delectus
+				sunt in temporibus omnis nesciunt expedita odio. Ut fugiat culpa quas
+				temporibus labore.
+			</p>
+			<p>
+				Voluptas placeat quis qui. Distinctio magnam fugit aut itaque possimus
+				sunt sit. Explicabo sint totam nesciunt. Nemo maiores ab maiores minus
+				doloremque a consequuntur labore. Molestiae laboriosam voluptas eos ut
+				nobis.
+			</p>
+			<p>
+				Aspernatur ut quidem sit magnam voluptatem. Quidem impedit sequi tempora
+				ad quibusdam ullam atque optio. Maiores quibusdam quae vero. Sed
+				corrupti eaque sint doloribus facilis id dolor. Enim tempora magnam
+				molestiae voluptatum sunt ipsam officiis est.
+			</p>
+		</>
+	);
+};
+
+export default About;
