851943fe81614bb71396940b2298517b254e19e6
amirmanshadi <amirmanshadi.code@gmail.com>
2023-04-02 18:19:03 +0430

    added help link to Navbar.js and removed to / from to props

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,8 @@ const Navbar = () => {
 				<NavLink to="/">jobarouter</NavLink>
 			</h1>
 			<NavLink className="button" to="/">home</NavLink>
-			<NavLink className="button" to="/about">about</NavLink>
+			<NavLink className="button" to="about">about</NavLink>
+			<NavLink className="button" to="help">help</NavLink>
 		</nav>
 	);
 };
