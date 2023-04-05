55444b7a8f452b9aed5a1de595c8289429175097
amirmanshadi <amirmanshadi.code@gmail.com>
2023-04-05 21:17:53 +0430

    created Careers.js component

---
+++ src/components/Careers.js
@@ -0,0 +1,21 @@
+import { Link, useLoaderData } from "react-router-dom";
+
+const Careers = () => {
+	const careers = useLoaderData();
+
+	return (
+		<>
+			{careers.map((career) => (
+				<Link
+					path="/"
+					key={career.id}
+				>
+					<h3>{career.title}</h3>
+					<p>{career.location}</p>
+				</Link>
+			))}
+		</>
+	);
+};
+
+export default Careers;
