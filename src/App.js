57c6a2635cd2a5ee085cce0a26073e88bc4e33b9
amirmanshadi <amirmanshadi.code@gmail.com>
2023-04-05 19:41:23 +0430

    changed (migrated) the whole routing system to new recommended version

--- src/App.js
+++ src/App.js
@@ -1,30 +1,27 @@
-import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom";
+import {
+	RouterProvider,
+	createBrowserRouter,
+	createRoutesFromElements,
+	Route,
+} from "react-router-dom";
 
 // pages
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Navbar from "./components/Navbar";
+
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
+    </Route>
+  )
+)
 
 function App() {
-	return (
-		<BrowserRouter>
-      <header>
-        <Navbar/>
-      </header>
-			<main>
-				<Routes>
-					<Route
-						index
-						element={<Home />}
-					/>
-					<Route
-						path="about"
-						element={<About />}
-					/>
-				</Routes>
-			</main>
-		</BrowserRouter>
-	);
+	return <RouterProvider router={router} />;
 }
 
 export default App;
