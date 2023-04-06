83b30ed804401474f6baba990180a7ba03354ebe
amirmanshadi <amirmanshadi.code@gmail.com>
2023-04-06 18:28:04 +0430

    created accordions in Faq.js

--- src/pages/Faq.js
+++ src/pages/Faq.js
@@ -1,9 +1,34 @@
+import Accordion from "../components/Accordion"
+
 const Faq = () => {
-  return ( 
-    <div className="faq">
-      faq
-    </div>
-   );
+	return (
+		<div className="faq">
+			<Accordion
+				question={"question 1"}
+				answer={
+					"Non consequuntur sit architecto omnis ad et. Modi porro asperiores quia et et voluptatem eligendi autem. Fuga et quod ratione maxime consequatur et excepturi. Porro nulla voluptatem hic et pariatur. Nisi eius nulla eos qui. Rerum vitae nobis iste."
+				}
+			/>
+			<Accordion
+				question={"question 2"}
+				answer={
+					"Non consequuntur sit architecto omnis ad et. Modi porro asperiores quia et et voluptatem eligendi autem. Fuga et quod ratione maxime consequatur et excepturi. Porro nulla voluptatem hic et pariatur. Nisi eius nulla eos qui. Rerum vitae nobis iste."
+				}
+			/>
+			<Accordion
+				question={"question 3"}
+				answer={
+					"Non consequuntur sit architecto omnis ad et. Modi porro asperiores quia et et voluptatem eligendi autem. Fuga et quod ratione maxime consequatur et excepturi. Porro nulla voluptatem hic et pariatur. Nisi eius nulla eos qui. Rerum vitae nobis iste."
+				}
+			/>
+			<Accordion
+				question={"question 4"}
+				answer={
+					"Non consequuntur sit architecto omnis ad et. Modi porro asperiores quia et et voluptatem eligendi autem. Fuga et quod ratione maxime consequatur et excepturi. Porro nulla voluptatem hic et pariatur. Nisi eius nulla eos qui. Rerum vitae nobis iste."
+				}
+			/>
+		</div>
+	)
 }
- 
-export default Faq;
+
+export default Faq
