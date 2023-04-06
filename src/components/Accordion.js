9a1a6445e9eb727cecb0f13948ae12aafe7a7d8a
amirmanshadi <amirmanshadi.code@gmail.com>
2023-04-06 18:56:28 +0430

    changed chevron png with svg

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,9 +1,7 @@
-import chevron from "../assets/icons8-chevron-left-96.png"
-
 const Accordion = ({ question, answer }) => {
 	function handleClick(e) {
 		const {
-			currentTarget: { parentElement: target }
+			currentTarget: { parentElement: target },
 		} = e
 		target.classList.toggle("collapse")
 	}
@@ -13,7 +11,9 @@ const Accordion = ({ question, answer }) => {
 			<div className="accordion-header" onClick={handleClick}>
 				<h4 className="accordion-title">{question}</h4>
 				<span className="accordion-icon">
-					<img src={chevron} />
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+						<path d="M233.4 105.4c12.5-12.5 32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L256 173.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l192-192z" />
+					</svg>
 				</span>
 			</div>
 			<div className="accordion-content">
