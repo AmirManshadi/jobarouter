0c5561da57b35364763b142c41e1a8ffdd878c52
amirmanshadi <amirmanshadi.code@gmail.com>
2023-04-07 18:37:24 +0430

    added exception handling to getCareers.js loader function

--- src/utils/getCareers.js
+++ src/utils/getCareers.js
@@ -1,6 +1,7 @@
 const getCareers = async () => {
-	const res = await fetch("http://localhost:5500/careers");
-	return res.json();
-};
+	const res = await fetch("http://localhost:5500/careers")
+	if (!res.ok) throw Error("could not fetch careers")
+	return res.json()
+}
 
-export default getCareers;
+export default getCareers
