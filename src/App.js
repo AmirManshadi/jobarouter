a6e11b30add50762e1a834edf6a2fc53a13eaa30
amirmanshadi <amirmanshadi.code@gmail.com>
2023-04-07 18:41:21 +0430

    created routing for CareerDetail.js and it's errorElement and loader

--- src/App.js
+++ src/App.js
@@ -3,53 +3,56 @@ import {
 	createBrowserRouter,
 	createRoutesFromElements,
 	Route,
-} from "react-router-dom";
+} from "react-router-dom"
 
 // components
-import Careers from "./components/Careers";
+import Careers from "./components/Careers"
 
 // pages
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Faq from "./pages/Faq";
-import ContactUs from "./pages/ContactUs";
-import NotFound from "./pages/NotFound";
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Faq from "./pages/Faq"
+import ContactUs from "./pages/ContactUs"
+import NotFound from "./pages/NotFound"
+import CareerDetail from "./pages/CareerDetail"
+import CareerError from "./pages/CareerError"
 
 // layouts
-import RootLayout from "./layouts/RootLayout";
-import HelpLayout from "./layouts/HelpLayout";
-import CareersLayout from "./layouts/CareersLayout";
+import RootLayout from "./layouts/RootLayout"
+import HelpLayout from "./layouts/HelpLayout"
+import CareersLayout from "./layouts/CareersLayout"
 
 // utilities
-import getCareers from "./utils/getCareers";
+import getCareers from "./utils/getCareers"
+import careerLoader from "./utils/careerLoader"
 
 const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<RootLayout />}>
+	createRoutesFromElements(
+		<Route path="/" element={<RootLayout />}>
+			<Route index element={<Home />} />
+			<Route path="about" element={<About />} />
 
-      <Route index element={<Home />} />
-      <Route path="about" element={<About />} />
+			<Route path="help" element={<HelpLayout />}>
+				<Route path="faq" element={<Faq />} />
+				<Route path="contact" element={<ContactUs />} />
+			</Route>
 
-      <Route path="help" element={<HelpLayout />}>
-        <Route path="faq" element={<Faq />} />
-        <Route path="contact" element={<ContactUs />} />
-      </Route>
+			<Route
+				path="careers"
+				element={<CareersLayout />}
+				errorElement={<CareerError />}
+			>
+				<Route index element={<Careers />} loader={getCareers} />
+				<Route path=":id" element={<CareerDetail />} loader={careerLoader} />
+			</Route>
 
-      <Route path="careers" element={<CareersLayout />}>
-        <Route 
-          index
-          element={<Careers />}  
-          loader={getCareers}
-        />
-      </Route>
-
-      <Route path="*" element={<NotFound />} />
-    </Route>
-  )
+			<Route path="*" element={<NotFound />} />
+		</Route>
+	)
 )
 
 function App() {
-	return <RouterProvider router={router} />;
+	return <RouterProvider router={router} />
 }
 
-export default App;
+export default App
