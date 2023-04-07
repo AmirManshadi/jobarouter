edda6b8c85a69e94327c123a177017fa27348f60
amirmanshadi <amirmanshadi.code@gmail.com>
2023-04-07 18:39:52 +0430

    added proper linking for careers

--- src/components/Careers.js
+++ src/components/Careers.js
@@ -1,21 +1,18 @@
-import { Link, useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom"
 
 const Careers = () => {
-	const careers = useLoaderData();
+	const careers = useLoaderData()
 
 	return (
 		<>
-			{careers.map((career) => (
-				<Link
-					to="/"
-					key={career.id}
-				>
+			{careers.map(career => (
+				<Link to={career.id.toString()} key={career.id}>
 					<h3>{career.title}</h3>
 					<p>{career.location}</p>
 				</Link>
 			))}
 		</>
-	);
-};
+	)
+}
 
-export default Careers;
+export default Careers
