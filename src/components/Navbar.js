d3bdeadaefabe03a8669b037ba6eba40983ef117
amirmanshadi <amirmanshadi.code@gmail.com>
2023-04-09 21:58:13 +0430

    added Breadcrumbs.js to Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,29 @@
-import { NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom"
+import Breadcrumbs from "./Breadcrumbs"
 
 const Navbar = () => {
 	return (
-		<nav>
-			<h1 className="logo">
-				<NavLink to="/">jobarouter</NavLink>
-			</h1>
-			<NavLink className="button" to="/">home</NavLink>
-			<NavLink className="button" to="about">about</NavLink>
-			<NavLink className="button" to="help">help</NavLink>
-			<NavLink className="button" to="careers">careers</NavLink>
-		</nav>
-	);
-};
+		<>
+			<nav>
+				<h1 className="logo">
+					<NavLink to="/">jobarouter</NavLink>
+				</h1>
+				<NavLink className="button" to="/">
+					home
+				</NavLink>
+				<NavLink className="button" to="about">
+					about
+				</NavLink>
+				<NavLink className="button" to="help">
+					help
+				</NavLink>
+				<NavLink className="button" to="careers">
+					careers
+				</NavLink>
+			</nav>
+			<Breadcrumbs />
+		</>
+	)
+}
 
-export default Navbar;
+export default Navbar
